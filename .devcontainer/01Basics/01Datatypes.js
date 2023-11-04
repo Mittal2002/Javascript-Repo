739d06c5016db48b63193fdb9cbde3e83952b73d
Mittal2002 <101242599+Mittal2002@users.noreply.github.com>
2023-11-04 06:31:05 +0000

    2 commit

---
+++ .devcontainer/01Basics/01Datatypes.js
@@ -0,0 +1,8 @@
+var name1="Palak";
+var age=32;
+var isgood=false;
+var obj='great';
+ var dholu;
+console.table([typeof name1,typeof age,typeof isgood ])
+console.log(typeof undefined);
+console.log(typeof null);
--- .devcontainer/01Basics/01Variables.js
+++ .devcontainer/01Basics/01Variables.js
@@ -1,5 +1,9 @@
 const accountNo="1234";
 let accountEmail="palak@gmail.com";
 var accountPassword="Jaipur";
-accountNo="2";
+accountEmail="aarzoo@gmail.com";
+accountPassword="Bengulur";
+accountNo1="2";
+accountNo1="5";
+console.table([accountEmail,accountNo,accountPassword,accountNo1]);
 console.log(accountNo);
